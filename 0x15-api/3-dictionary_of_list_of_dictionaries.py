a06b31330ec23c11a20919cbf60ec5b7df410187
Peter Chibunna <peterchibunna@gmail.com>
2023-02-08 12:14:38 +0100

    Fixed PEP8 validation for Task #3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,7 +10,9 @@ Format must be: { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
 "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
 "task": "TASK_TITLE",
 "completed": TASK_COMPLETED_STATUS}, ... ],
-"USER_ID": [{"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
+"USER_ID": [{"username": "USERNAME", "task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS}, ... ]}
 File name must be: todo_all_employees.json
 Example:
 """
